15016635dd9c06e53dfd1cc17d21fe8f4ce45e2b
C <142123173+cjbuisness@users.noreply.github.com>
2023-11-28 14:55:48 -0800

    Update Main.java

--- package Tree/Main.java
+++ package Tree/Main.java
@@ -11,19 +11,17 @@ public class Main {
         BinarySearchTrees<String> reservedWordsBST = new BinarySearchTrees<>();
         BinarySearchTrees<String> identifiersBST = new BinarySearchTrees<>();
 
-     /**
-	* reads Palindrome.java
-	* NOTE!!!! 
-	* as you can tell, the file path is custom to my machine
-	* update the file path for your Palindrome class, or another
-	**/
+        /**
+         * reads Palindrome.java
+         * NOTE!!!! as you can tell, the file path is custom to my machine update the file path for your Palindrome class, or another
+         **/
         JavaProgramToBeTested javaProgram = readJavaProgram("C:\\Users\\Default.DESKTOP-JMBKVIA\\Desktop\\Palindrome.java");
 
         // initializes reservedwords BST
         initializeReservedWords(reservedWordsBST);
 
         // parses identifiers from Palindrome.java, and adds to identifiers BST
-        parseIdentifiers(javaProgram.getContent(), identifiersBST);
+        parseIdentifiers(javaProgram.getContent(), identifiersBST, reservedWordsBST);
 
         // prints in-order traversal of the reserved words Binary Search Tree
         printInOrderTraversal("In-order traversal of Reserved Words BST:", reservedWordsBST);
@@ -34,9 +32,7 @@ public class Main {
 
     /**
      * initializes reservedwords BST from reservedWords.txt
-     * NOTE!!!! 
-     * as you can tell, the file path is custom to my machine
-     * update the file path to your machine
+     * NOTE!!!! as you can tell, the file path is custom to my machine update the file path to your machine
      **/
     private static void initializeReservedWords(BinarySearchTrees<String> reservedWordsBST) {
         try (BufferedReader reader = new BufferedReader(new FileReader("C:\\Users\\Default.DESKTOP-JMBKVIA\\Desktop\\reservedWords.txt", StandardCharsets.UTF_8))) {
@@ -50,26 +46,26 @@ public class Main {
         StringBuilder contentBuilder = new StringBuilder();
         try (BufferedReader reader = new BufferedReader(new FileReader(filePath, StandardCharsets.UTF_8))) {
             reader.lines().forEach(line -> contentBuilder.append(line).append("\n"));
-	// reads Palindrome.java, and reutrns a JavaProgramToBeTested object from it 
+            // reads Palindrome.java, and returns a JavaProgramToBeTested object from it
         } catch (IOException e) {
             e.printStackTrace();
         }
         return new JavaProgramToBeTested(contentBuilder.toString());
     }
 
-    private static void parseIdentifiers(String javaProgramContent, BinarySearchTrees<String> identifiersBST) {
-	// parses identfiers from palindrome.java, and adds to BST
+    private static void parseIdentifiers(String javaProgramContent, BinarySearchTrees<String> identifiersBST, BinarySearchTrees<String> reservedWordsBST) {
+        // parses identifiers from palindrome.java, and adds to BST
         String[] words = javaProgramContent.split("\\W+");
         for (String word : words) {
-            // checks if first character is a letter
-            if (Character.isLetter(word.charAt(0))) {
+            // checks if first character is a letter and if the word is not in reservedWordsBST
+            if (Character.isLetter(word.charAt(0)) && !reservedWordsBST.contains(word)) {
                 identifiersBST.add(word);
             }
         }
     }
 
     private static void printInOrderTraversal(String message, BinarySearchTrees<String> bst) {
-	// prints in-order traversal
+        // prints in-order traversal
         System.out.println(message);
         bst.inOrderTraversal();
     }
