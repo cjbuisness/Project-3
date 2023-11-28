12a38aa42f18b3e675b85590fe5d786ced280fb1
C <142123173+cjbuisness@users.noreply.github.com>
2023-11-27 21:55:49 -0800

    Create JavaProgramToBeTested.java

---
+++ package Tree/JavaProgramToBeTested.java
@@ -0,0 +1,14 @@
+package Tree;
+
+public class JavaProgramToBeTested {
+    private final String content;
+
+    public JavaProgramToBeTested(String content) {
+        this.content = content;
+	// creates instance of class, with given content
+    }
+
+    public String getContent() {
+        return content;
+    }
+}
